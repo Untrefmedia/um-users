68bf1bf46b7d512a4245a682ade1672d38debb8b
pablo <pcoronel@untref.edu.ar>
2019-01-16 13:04:07 -0300

    renombrado ruta dataList

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,5 +2,5 @@
 
 Route::group(['prefix' => 'admin', 'middleware' => ['web', 'admin', 'auth:admin']], function () {
     Route::resource('user', 'Untrefmedia\UMUsers\App\Http\Controllers\Admin\UserController');
-    Route::get('user.dataList', 'Untrefmedia\UMUsers\App\Http\Controllers\Admin\UserController@dataList')->name('user.dataList');
+    Route::get('userDataList', 'Untrefmedia\UMUsers\App\Http\Controllers\Admin\UserController@dataList')->name('user.dataList');
 });
